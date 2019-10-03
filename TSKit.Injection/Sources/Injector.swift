361ccd6dc1778fa0d9a607370536e33cbe22e553
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2019-10-03 15:29:59 +0300

    Added type inference to injection.

--- TSKit.Injection/Sources/Injector.swift
+++ TSKit.Injection/Sources/Injector.swift
@@ -16,55 +16,100 @@ import TSKit_Log
  - Author:     AdYa
  */
 public class Injector {
-    
+
+    fileprivate typealias Rules = [String: [String: [String: InjectionRule]]]
+    fileprivate typealias Cache = [String: [String: [String: Any]]]
+
     /// Internal property to store configured injection rules.
-    fileprivate static var rules: [String : [String : [String : InjectionRule]]] = [:]
-    
-    fileprivate static var cache: [String : [String : [String : Any]]] = [:]
-    
-    static let log: AnyLogger = {
+    private static var rules: Rules = [:]
+
+    private static var cache: Cache = [:]
+
+    private static let synchronizationQueue = DispatchQueue(label: "Injector.synchronization_queue", qos: .background, attributes: .concurrent)
+
+    /// Flag indicating whether `Injector` should output its debug logs to the console.
+    public static var isDebugLoggingEnabled: Bool {
+        get {
+            return logger.writers.count > 0
+        }
+        set {
+            if newValue {
+                logger.writers.append(PrintLogEntryWriter(decorator: LogEntryDecorator(style: .iconic)))
+            } else {
+                logger.writers.removeAll()
+            }
+        }
+    }
+
+    private static let logger: Logger = {
         let log = Logger()
         return log
     }()
-    
+
+    static var log: AnyLogger {
+        return logger
+    }
+
+    fileprivate static var synchronizedRules: Rules {
+        get {
+            return synchronizationQueue.sync { rules }
+        }
+        set {
+            synchronizationQueue.async(flags: .barrier) { rules = newValue }
+        }
+    }
+
+    fileprivate static var synchronizedCache: Cache {
+        get {
+            return synchronizationQueue.sync { cache }
+        }
+        set {
+            synchronizationQueue.async(flags: .barrier) { cache = newValue }
+        }
+    }
+
     /// Replaces existing `InjectionRule`s with specified in the preset.
     /// - Parameter preset: An array of rules to be set.
-    public static func configure(with preset: InjectionPreset) {
+    public static func configure(with preset: InjectionRulesPreset) {
         self.configure(with: preset.rules)
     }
-    
+
     /// Replaces existing `InjectionRule`s with specified.
     /// - Parameter rules: An array of rules to be set.
     public static func configure(with rules: [InjectionRule]) {
-        rules.forEach { self.add($0) }
+        rules.forEach(add(rule:))
     }
-    
+
+    public static func addInjectionRule(_ rule: InjectionRule) {
+        add(rule: rule)
+    }
+
     /// Adds a single `InjectionRule` to existing rules.
     /// - Parameter rule: A rule to be added.
-    public static func add(_ rule : InjectionRule) {
+    private static func add(rule: InjectionRule) {
         let protocolType = String(describing: rule.protocolType)
         let targetType = String(describing: rule.targetType)
         let destinationType = String(describing: rule.destinationType)
-        
+
         // Either add to existing sub-dictionary or create new one
-        guard self.rules[protocolType] != nil else {
-            self.rules[protocolType] = [targetType : [destinationType : rule]]
+        guard rules[protocolType] != nil else {
+            rules[protocolType] = [targetType: [destinationType: rule]]
             return
         }
-        
-        guard self.rules[protocolType]?[targetType] != nil else {
-            self.rules[protocolType]?[targetType] = [destinationType : rule]
+
+        guard rules[protocolType]?[targetType] != nil else {
+            rules[protocolType]?[targetType] = [destinationType: rule]
             return
         }
-        
-        self.rules[protocolType]?[targetType]?[destinationType] = rule
+
+        rules[protocolType]?[targetType]?[destinationType] = rule
     }
-    
+
     /// Resets all previously configured rules.
     public static func reset() {
-        rules.removeAll()
+        synchronizedRules.removeAll()
     }
-    
+
     /**
      Injects concrete type conformed to target injectable type.
      - Parameter injectable: Protocol to which injected instance conforms.
@@ -79,45 +124,49 @@ public class Injector {
                                               with parameter: Any? = nil,
                                               for sender: Any.Type) throws -> InjectableType where InjectableType: Any {
         let target: Any.Type = parameter != nil ? type(of: parameter!) : Any.Type.self
-        
+
         let protocolType = String(describing: injectable)
         let targetType = String(describing: target)
         let destinationType = String(describing: sender)
         let defaultType = String(describing: Any.Type.self)
-        
+
+        let rules = synchronizedRules
+
         // get rules for specific target or default
         // get rule for specific destination or default
-        guard let targetRules = self.rules[protocolType]?[targetType] ?? self.rules[protocolType]?[defaultType],
-            let rule = targetRules[destinationType] ?? targetRules[defaultType]
-            else {
-                log.severe("Didn't find any rule suitable for injection of '\(protocolType)' with parameter '\(targetType)' for '\(destinationType)'.")
-                throw InjectionError.undefinedInjectionError
-        }
-        
-        if rule.once,
-            let targetCache = self.cache[protocolType]?[targetType] ?? self.cache[protocolType]?[defaultType],
-            let cached = (targetCache[destinationType] ?? targetCache[defaultType] as Any) as? InjectableType {
-            log.debug("Restored cached '\(protocolType)' with '\(type(of: cached))'.")
-            return cached
-        }
-        
-        guard let injected = try rule.injection(parameter) as? InjectableType else {
-            log.severe("'\(protocolType)' injection failed.")
+        guard let targetRules = rules[protocolType]?[targetType] ?? rules[protocolType]?[defaultType],
+              let rule = targetRules[destinationType] ?? targetRules[defaultType]
+                else {
+            log.severe("Didn't find any rule suitable for injection of \(protocolType) with parameter \(targetType) for \(destinationType)")
             throw InjectionError.undefinedInjectionError
         }
-        log.debug("Successfully injected '\(protocolType)' with '\(type(of: injected))'.")
+
+        let cache = synchronizedCache
+
+        if rule.once,
+           let targetCache = cache[protocolType]?[targetType] ?? cache[protocolType]?[defaultType],
+           let cached = (targetCache[destinationType] ?? targetCache[defaultType] as Any) as? InjectableType {
+            log.debug("Restored cached \(protocolType) with \(type(of: cached))")
+            return cached
+        }
+
+        guard let injected = try rule.injection(parameter) as? InjectableType else {
+            log.severe("\(protocolType) injection failed")
+            throw InjectionError.undefinedInjectionError
+        }
+        log.debug("Successfully injected \(protocolType) with \(type(of: injected))")
         if rule.once {
-            if self.cache[protocolType] == nil {
-                self.cache[protocolType] = [targetType : [destinationType : injected]]
-            } else if self.cache[protocolType]?[targetType] == nil {
-                self.cache[protocolType]?[targetType] = [destinationType : injected]
+            if cache[protocolType] == nil {
+                synchronizedCache[protocolType] = [targetType: [destinationType: injected]]
+            } else if cache[protocolType]?[targetType] == nil {
+                synchronizedCache[protocolType]?[targetType] = [destinationType: injected]
             } else {
-                self.cache[protocolType]?[targetType]?[destinationType] = injected
+                synchronizedCache[protocolType]?[targetType]?[destinationType] = injected
             }
         }
         return injected
     }
-    
+
     /**
      Injects concrete type conformed to target injectable type.
      - Parameter injectable: Protocol to which injected instance conforms.
@@ -134,13 +183,19 @@ public class Injector {
         let sender = sender.flatMap { type(of: $0) } ?? Any.Type.self
         return try inject(injectable, with: parameter, for: sender)
     }
-    
+
+    public static func inject<InjectableType: Any>(with parameter: Any? = nil,
+                                                   for sender: Any? = nil) throws -> InjectableType {
+        return try inject(InjectableType.self, with: parameter, for: sender)
+    }
+
     /// Prints all configured injection rules.
     public static func printConfiguration() {
         print("Configured injection rules: \n")
-        self.rules
-            .flatMap { $0.1.values }.flatMap { $0.values }
-            .sorted { "\($0.protocolType)".compare("\($1.protocolType)") == .orderedAscending }
-            .forEach { print("\($0)") }
+        self.synchronizedRules
+                .flatMap { $0.1.values }.flatMap { $0.values }
+                .sorted { "\($0.protocolType)".compare("\($1.protocolType)") == .orderedAscending }
+                .forEach { print("\($0)") }
     }
 }
+
